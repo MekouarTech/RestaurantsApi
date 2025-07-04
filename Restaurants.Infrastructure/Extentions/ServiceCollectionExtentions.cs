90ad1da8ca7576180478c13b5e94a9058c10d53a
Hamza Mekouar <mekouar.hamza1@gmail.com>
2025-07-04 12:44:03 +0100

    Turn to Flexible database connection

---
+++ Restaurants.Infrastructure/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Extentions;
+
+public static class ServiceCollectionExtentions
+{
+    public static void AddInfrastructure(this IServiceCollection services,IConfigurationManager configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString));
+    }
+}
